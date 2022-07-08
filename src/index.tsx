071462002aaf1d7c3b79b70ee9f6056ef14783ba
Rinkichi75 <cocmithulan@gmail.com>
2022-07-08 15:59:20 +0200

    FEAT/ index

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { MainNavigator } from "./modules/navigation/pages";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MainNavigator />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
